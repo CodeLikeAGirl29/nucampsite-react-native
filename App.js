33875f98b9739b162573ab19f5c9647713d82608
Lindsey <miss_leading009@yahoo.com>
2021-11-15 07:28:17 -0600

    Icons, Favorites, and Comments

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StyleSheet, Text, View, YellowBox } from "react-native";
 import Main from "./components/MainComponent";
+YellowBox.ignoreWarnings([""]);
 
 export default function App() {
   return <Main />;
